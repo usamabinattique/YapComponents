0b9f5348f205f9bf87d79ba7d7a7c9be076b7a8a
Tayyab Akram <tayyab.akram@digitify.com>
2021-08-09 15:39:34 +0500

    Added background color in text field

--- Source/DefaultTextField.swift
+++ Source/DefaultTextField.swift
@@ -9,4 +9,17 @@ import UIKit
 
 open class DefaultTextField: UITextField {
 
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    private func setup() {
+        backgroundColor = UIColor(white: 0.0, alpha: 0.05)
+    }
 }
