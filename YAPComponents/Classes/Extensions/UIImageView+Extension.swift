41fe0c58b876fa0ac37a18ace0e8b459046dec8f
Sarmad <sarmad.abbas@digitify.com>
2021-08-30 09:56:48 +0500

    UIImage Extensions, UIView Factory Method

---
+++ YAPComponents/Classes/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+Extension.swift
+//  YAPComponents
+//
+//  Created by Sarmad on 30/08/2021.
+//
+
+import UIKit
+
+extension UIImageView {
+    @discardableResult func setImage(_ name:String,
+                                     rendringMode:UIImage.RenderingMode) -> Self {
+        image = UIImage(named: name)?.withRenderingMode(rendringMode)
+        return self
+    }
+    
+    @discardableResult func setImage(_ image:UIImage?,
+                                     rendringMode:UIImage.RenderingMode) -> Self {
+        self.image = image?.withRenderingMode(rendringMode)
+        return self
+    }
+}
