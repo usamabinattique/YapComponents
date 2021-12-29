06f217a76f2fc840c9202c6ede2491082a094233
Sarmad <sarmad.abbas@digitify.com>
2021-12-30 02:21:27 +0500

    YAPCheckBox added

---
+++ YAPComponents/Classes/Components/YAPCheckBox.swift
@@ -0,0 +1,92 @@
+//
+//  YAPCheckBox.swift
+//  YAPKit
+//
+//  Created by Zain on 28/10/2019.
+//  Copyright © 2019 YAP. All rights reserved.
+//
+
+import Foundation
+
+public class YAPCheckBox: UIControl {
+    
+    // MARK: Views
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.isHidden = true
+        imageView.image = UIImage.sharedImage(named: "icon_check")?.asTemplate
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    // MARK: Properties
+    
+    public var fillColor: UIColor = .blue
+    
+    public var checked: Bool = false {
+        didSet {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.backgroundColor = self.checked ? self.fillColor : .clear
+                self.layer.borderColor = self.checked ? UIColor.clear.cgColor : UIColor.gray.cgColor
+                self.imageView.isHidden = !self.checked
+            })
+        }
+    }
+    
+    public override var tintColor: UIColor! {
+        didSet {
+            imageView.tintColor = tintColor
+        }
+    }
+    
+    // MARK: Initialization
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        tintColor = .white
+        backgroundColor = .clear
+        setupViews()
+        setupConstraints()
+        render()
+        
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
+    }
+    
+    // MARK: Actions
+    
+    @objc
+    func tapped() {
+        checked = !checked
+        sendActions(for: .valueChanged)
+    }
+}
+
+// MARK: View setup
+
+private extension YAPCheckBox {
+    func setupViews() {
+        addSubview(imageView)
+    }
+    
+    func setupConstraints() {
+        imageView.alignEdgesWithSuperview([.left, .top, .right, .bottom], constant: 4)
+    }
+    
+    func render() {
+        layer.cornerRadius = 4
+        layer.borderColor = UIColor.gray.cgColor
+        layer.borderWidth = 1
+        clipsToBounds = true
+    }
+}
