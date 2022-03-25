0808c5279661a5428fd79a1809e887d9038b9dde
Uzair Jawad <>
2022-03-25 15:05:53 +0500

    - string extension some func added.

--- YAPComponents/Classes/Extensions/String+Extension.swift
+++ YAPComponents/Classes/Extensions/String+Extension.swift
@@ -7,7 +7,21 @@
 
 import UIKit
 
-extension String {
+public extension String {
     var length:Int { return (self as NSString).length }
     var range:NSRange { return NSRange(location: 0, length:self.length)}
+    
+    func replace(string: String, replacement: String) -> String {
+        return self.replacingOccurrences(of: string, with: replacement, options: String.CompareOptions.literal, range: nil)
+    }
+    
+    func removingGroupingSeparator() -> String {
+        return self.replace(string: localeNumberFormatter.currencyGroupingSeparator, replacement: "")
+    }
 }
+
+public var localeNumberFormatter: NumberFormatter = {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.locale = .current
+    return numberFormatter
+}()
